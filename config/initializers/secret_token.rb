f6112a92a5d58307c297eeba6672cd0fdbfd10f0
Michael Sarlitt <mbsarlitt@gmail.com>
2013-11-09 17:57:06 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gapper::Application.config.secret_key_base = '2e338615dfde6ee85ed6a9ea30d34e9432ceeae5627c540a4caf53127015950b578b959fdcf539f12ecd5455d52de69b13ae15f0ab8b9095d47e4a0d24ca5a12'
